79372cb8db057be9b5a500ca51aae1a8ad777b53
ommiputera <omiputrakarunia@gmail.com>
2023-05-25 13:28:10 +0700

    update: loader

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,9 +1,26 @@
-import type { V2_MetaFunction } from "@remix-run/react";
+import type {Joke} from '@prisma/client'
+import {type V2_MetaFunction, useLoaderData} from '@remix-run/react'
+import {db} from '~/utils/db.server'
+
+type LoaderData = {jokes: Array<Joke>}
+
+export const loader = async () => {
+  let jokes = await db.joke.findMany()
+  let data: LoaderData = {jokes}
+  return data
+}
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "Post pages" }];
-};
+  return [{title: 'Post pages'}]
+}
 
 export default function Index() {
-  return <div>Index</div>;
+  const data = useLoaderData<LoaderData>()
+  return (
+    <div>
+      {data.jokes.map(joke => (
+        <p key={joke.id}>{joke.name}</p>
+      ))}
+    </div>
+  )
 }
