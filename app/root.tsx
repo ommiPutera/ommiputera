d867f2f547549024abf37704f80a57d1d5b38737
ommiputera <omiputerakarunia@gmail.com>
2023-05-27 00:15:15 +0700

    postgresql

--- app/root.tsx
+++ app/root.tsx
@@ -73,23 +73,6 @@ export const links: LinksFunction = () => {
       type: 'font/woff2',
       crossOrigin: 'anonymous',
     },
-    {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      href: '/favicons/apple-touch-icon.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      href: '/favicons/favicon-32x32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      href: '/favicons/favicon-16x16.png',
-    },
     {rel: 'stylesheet', href: appStyles},
     {rel: 'stylesheet', href: tailwindStyles},
     {rel: 'stylesheet', href: vendorsStyles},
