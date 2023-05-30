48e2daa10f266b2e2d8f247a43b7eadb618f0687
ommiputera <omiputrakarunia@gmail.com>
2023-05-30 16:57:49 +0700

    update: fixing mobile size

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,8 @@ export default function Index() {
       <div className="flex flex-col gap-8 bg-gray-800 px-5vw py-9 lg:px-15vw">
         <h3 className="text-center text-xl font-medium">Certified By</h3>
         <div className="mx-auto flex max-w-8xl items-center justify-center gap-x-9">
-          <img src="/purwadhika-logo.png" alt="" className="w-52" />
-          <img src="/udemy-logo.png" alt="" className="w-36" />
+          <img src="/purwadhika-logo.png" alt="" className="w-32 lg:w-52" />
+          <img src="/udemy-logo.png" alt="" className="w-24 lg:w-36" />
         </div>
       </div>
       <div className="px-5vw py-9 lg:px-15vw lg:py-12">
