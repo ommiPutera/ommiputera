2412d4c5705b24520253e184b78d54585610ad88
ommiputera <omiputrakarunia@gmail.com>
2023-06-12 14:56:35 +0700

    update page loading handler

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,8 @@ import {
   type SerializeFrom,
   type V2_MetaFunction,
 } from '@remix-run/node'
+import * as React from 'react'
+import { useSpinDelay } from 'spin-delay'
 import {
   Links,
   LiveReload,
@@ -12,62 +14,26 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigation,
 } from '@remix-run/react'
-import {Analytics} from '@vercel/analytics/react'
-import {Navbar} from '~/components/navbar'
+import { Analytics } from '@vercel/analytics/react'
+import { Navbar } from '~/components/navbar'
 import appStyles from '~/styles/app.css'
 import tailwindStyles from '~/styles/tailwind.css'
 import vendorsStyles from '~/styles/vendors.css'
-import {ThemeProvider, useTheme} from '~/utils/theme-provider'
+import { ThemeProvider, useTheme } from '~/utils/theme-provider'
 import Footer from './components/footer'
-import {getDomainUrl, getUrl} from './utils/misc'
-import {getSocialMetas} from './utils/seo'
-import {getUser} from './utils/session.server'
-
-export default function AppWithProviders() {
-  return (
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  )
-}
-
-function App() {
-  const [theme] = useTheme()
-  return (
-    <html lang="en" className={`${theme}`}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="theme-color"
-          content={theme === 'dark' ? '#161513' : '#FFF'}
-        />
-        <Meta />
-        <Links />
-      </head>
-      <body className="bg-white transition duration-500 dark:bg-gray-900">
-        <Navbar />
-        <Outlet />
-        <Footer />
-        <Analytics />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  )
-}
+import { getDomainUrl, getUrl } from './utils/misc'
+import { getSocialMetas } from './utils/seo'
+import { getUser } from './utils/session.server'
 
 export type LoaderData = SerializeFrom<typeof loader>
 
-export const handle: {id: string} = {
+export const handle: { id: string } = {
   id: 'root',
 }
 
-export async function loader({request}: DataFunctionArgs) {
+export async function loader({ request }: DataFunctionArgs) {
   const user = await getUser(request)
   const data = {
     user,
@@ -76,17 +42,17 @@ export async function loader({request}: DataFunctionArgs) {
     },
   }
   const headers: HeadersInit = new Headers()
-  return json(data, {headers})
+  return json(data, { headers })
 }
 
-export const meta: V2_MetaFunction = ({data}) => {
+export const meta: V2_MetaFunction = ({ data }) => {
   const requestInfo = data?.requestInfo
   const socials = getSocialMetas({
     keywords: 'Personal Website',
     url: getUrl(requestInfo),
   })
 
-  return [{title: 'Ommi Putera - Personal Website'}, ...socials]
+  return [{ title: 'Ommi Putera - Personal Website' }, ...socials]
 }
 
 export const links: LinksFunction = () => {
@@ -112,8 +78,104 @@ export const links: LinksFunction = () => {
       type: 'font/woff2',
       crossOrigin: 'anonymous',
     },
-    {rel: 'stylesheet', href: vendorsStyles},
-    {rel: 'stylesheet', href: tailwindStyles},
-    {rel: 'stylesheet', href: appStyles},
+    { rel: 'stylesheet', href: vendorsStyles },
+    { rel: 'stylesheet', href: tailwindStyles },
+    { rel: 'stylesheet', href: appStyles },
   ]
+}
+
+export default function AppWithProviders() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  )
+}
+
+const LOADER_WORDS = [
+  'Loading page..',
+  'checking cdn',
+  'checking cache',
+  'fetching from db',
+  'compiling mdx',
+  'updating cache',
+  'transfer',
+]
+
+const ACTION_WORDS = [
+  'packaging',
+  'zapping',
+  'validating',
+  'processing',
+  'calculating',
+  'computing',
+  'computering',
+]
+
+let firstRender = true
+function PageLoadingMessage() {
+  const navigation = useNavigation()
+  const [words, setWords] = React.useState<Array<string>>([])
+  const [pendingPath, setPendingPath] = React.useState('')
+  const showLoader = useSpinDelay(Boolean(navigation.state !== 'idle'), {
+    delay: 400,
+    minDuration: 1000,
+  })
+
+  React.useEffect(() => {
+    if (firstRender) return
+    if (navigation.state === 'idle') return
+    if (navigation.state === 'loading') setWords(LOADER_WORDS)
+    if (navigation.state === 'submitting') setWords(ACTION_WORDS)
+
+    const interval = setInterval(() => {
+      setWords(([first, ...rest]) => [...rest, first] as Array<string>)
+    }, 2000)
+
+    return () => clearInterval(interval)
+  }, [navigation.state])
+
+  React.useEffect(() => {
+    if (firstRender) return
+    if (navigation.state === 'idle') return
+    setPendingPath(navigation.location.pathname)
+  }, [navigation])
+
+  React.useEffect(() => {
+    firstRender = false
+  }, [])
+
+  const action = words[0]
+
+  return <div hidden={!showLoader} className="fixed top-14 bg-red-500 left-0 right-0 w-fit mx-auto px-4 pb-1.5 rounded-full font-medium text-md z-50">{action}</div>
+}
+
+function App() {
+  const [theme] = useTheme()
+  return (
+    <html lang="en" className={`${theme}`}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="theme-color"
+          content={theme === 'dark' ? '#161513' : '#FFF'}
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-white transition duration-500 dark:bg-gray-900">
+        <PageLoadingMessage />
+        <Navbar />
+        <Outlet />
+        <Footer />
+        <Analytics />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
 }
