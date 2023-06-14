be12c6fa9909953b247afae50950b754d2a72732
ommiputera <omiputerakarunia@gmail.com>
2023-06-15 01:50:53 +0700

    format

--- app/routes/admin.manage-project.form.tsx
+++ app/routes/admin.manage-project.form.tsx
@@ -1,6 +1,6 @@
-import type { Project } from '@prisma/client'
-import type { ActionFunction } from '@remix-run/node';
-import { type LoaderFunction } from '@remix-run/node'
+import type {Project} from '@prisma/client'
+import type {ActionFunction} from '@remix-run/node'
+import {type LoaderFunction} from '@remix-run/node'
 import {
   Form,
   Link,
@@ -10,13 +10,13 @@ import {
 } from '@remix-run/react'
 import clsx from 'clsx'
 import React from 'react'
-import { Button } from '~/components/button'
-import { Input, Label } from '~/components/form-elements'
-import { db } from '~/utils/db.server'
-import { createProject, updateProject } from '~/utils/project.session';
-import { getUserId } from '~/utils/session.server';
+import {Button} from '~/components/button'
+import {Input, Label} from '~/components/form-elements'
+import {db} from '~/utils/db.server'
+import {createProject, updateProject} from '~/utils/project.session'
+import {getUserId} from '~/utils/session.server'
 
-type LoaderData = { project: Project | null }
+type LoaderData = {project: Project | null}
 type ActionData = {
   formError?: string
   fieldErrors?: {
@@ -34,25 +34,25 @@ type ActionData = {
 }
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: 'Admin Panel - Form' }]
+  return [{title: 'Admin Panel - Form'}]
 }
 
-async function getLoaderData({ request }: { request: Request }) {
-  const { searchParams } = new URL(request.url)
+async function getLoaderData({request}: {request: Request}) {
+  const {searchParams} = new URL(request.url)
   const id = searchParams.get('id')
-  const project = await db.project.findUnique({ where: { id: id ?? '' } })
+  const project = await db.project.findUnique({where: {id: id ?? ''}})
   return project
 }
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const project = await getLoaderData({ request })
-  let data: LoaderData = { project }
+export const loader: LoaderFunction = async ({request}) => {
+  const project = await getLoaderData({request})
+  let data: LoaderData = {project}
   return data
 }
 
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({request}) => {
   const formData = await request.formData()
-  const actionType = formData.get("actionType");
+  const actionType = formData.get('actionType')
   const projectId = formData.get('projectId')
   const projectName = formData.get('projectName')
   const type = formData.get('type')
@@ -67,21 +67,21 @@ export const action: ActionFunction = async ({ request }) => {
     typeof projectId !== 'string' ||
     typeof actionType !== 'string'
   ) {
-    return { formError: 'Form not submitted correctly' }
+    return {formError: 'Form not submitted correctly'}
   }
 
-  let fields = { projectName, description, type, heroId, userId }
+  let fields = {projectName, description, type, heroId, userId}
   switch (actionType) {
-    case "create": {
-      const project = await createProject({ ...fields })
+    case 'create': {
+      const project = await createProject({...fields})
       return project
     }
-    case "updateAndRead": {
-      const project = await updateProject({ projectId: projectId, ...fields })
+    case 'updateAndRead': {
+      const project = await updateProject({projectId: projectId, ...fields})
       return project
     }
     default: {
-      return { fields, formError: `Login type invalid` };
+      return {fields, formError: `Login type invalid`}
     }
   }
 }
@@ -97,19 +97,22 @@ export default function Index() {
     <div className="px-6 py-2">
       <div className="flex items-center justify-between">
         <div className="w-min">
-          <Link to="/admin/manage-project" prefetch='intent'>
+          <Link to="/admin/manage-project" prefetch="intent">
             <Button size="md">
-              <span className='text-md mr-2'>↩️</span>
+              <span className="mr-2 text-md">↩️</span>
               Back
             </Button>
           </Link>
         </div>
-        <div className={clsx("rounded-lg border px-4 py-2.5", {
-          "border-green-200 bg-green-100 text-green-900": isCreate,
-          "border-orange-300 bg-orange-100 text-orange-800": isUpdateAndRead,
-        })}
+        <div
+          className={clsx('rounded-lg border px-4 py-2.5', {
+            'border-green-200 bg-green-100 text-green-900': isCreate,
+            'border-orange-300 bg-orange-100 text-orange-800': isUpdateAndRead,
+          })}
         >
-          <h1 className="text-md">{isUpdateAndRead ? "Update" : "Create"} New Project</h1>
+          <h1 className="text-md">
+            {isUpdateAndRead ? 'Update' : 'Create'} New Project
+          </h1>
         </div>
       </div>
       <div className="pb-6 pt-12">
@@ -130,7 +133,7 @@ function FormAction() {
     description: '',
     type: '',
     heroId: '',
-    actionType: ''
+    actionType: '',
   })
 
   const isCreate = Boolean(!project)
@@ -150,7 +153,7 @@ function FormAction() {
           description: form.description.value,
           type: form.type.value,
           heroId: form.heroId.value,
-          actionType: actionType()
+          actionType: actionType(),
         })
         setSubmitted(false)
       }}
@@ -159,9 +162,9 @@ function FormAction() {
       onSubmit={() => setSubmitted(true)}
     >
       <input type="hidden" name="actionType" value={actionType()} />
-      <input type="hidden" name="projectId" value={project?.id || ""} />
-      <div className='grid grid-cols-3 gap-x-4'>
-        <div className='mb-3 col-span-1'>
+      <input type="hidden" name="projectId" value={project?.id || ''} />
+      <div className="grid grid-cols-3 gap-x-4">
+        <div className="col-span-1 mb-3">
           <div className="mb-1.5 flex flex-wrap items-baseline justify-between">
             <Label htmlFor="name-field">Name</Label>
           </div>
@@ -178,7 +181,7 @@ function FormAction() {
             }
           />
         </div>
-        <div className='mb-3 col-span-1'>
+        <div className="col-span-1 mb-3">
           <div className="mb-1.5 flex flex-wrap items-baseline justify-between">
             <Label htmlFor="name-field">type</Label>
           </div>
@@ -189,13 +192,11 @@ function FormAction() {
             placeholder="type"
             id="type-field"
             aria-describedby={
-              actionData?.fieldErrors?.type
-                ? 'type-error'
-                : undefined
+              actionData?.fieldErrors?.type ? 'type-error' : undefined
             }
           />
         </div>
-        <div className='mb-3 col-span-1'>
+        <div className="col-span-1 mb-3">
           <div className="mb-1.5 flex flex-wrap items-baseline justify-between">
             <Label htmlFor="name-field">Description</Label>
           </div>
@@ -213,8 +214,8 @@ function FormAction() {
           />
         </div>
       </div>
-      <div className='grid grid-cols-3 gap-x-4'>
-        <div className='mb-3 col-span-1'>
+      <div className="grid grid-cols-3 gap-x-4">
+        <div className="col-span-1 mb-3">
           <div className="mb-1.5 flex flex-wrap items-baseline justify-between">
             <Label htmlFor="name-field">heroId</Label>
           </div>
@@ -225,22 +226,16 @@ function FormAction() {
             placeholder="heroId"
             id="heroId-field"
             aria-describedby={
-              actionData?.fieldErrors?.heroId
-                ? 'heroId-error'
-                : undefined
+              actionData?.fieldErrors?.heroId ? 'heroId-error' : undefined
             }
           />
         </div>
       </div>
-      <div className='w-full flex justify-end'>
-        <div className='w-min'>
-          <Button
-            type="submit"
-            size="md"
-            className="mt-4"
-          >
-            {isCreate ? "Create" : "Update"}
-            <span className='ml-2 text-md'>💣</span>
+      <div className="flex w-full justify-end">
+        <div className="w-min">
+          <Button type="submit" size="md" className="mt-4">
+            {isCreate ? 'Create' : 'Update'}
+            <span className="ml-2 text-md">💣</span>
           </Button>
         </div>
       </div>
