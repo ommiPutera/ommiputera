e56d937ed07693c0c43255f3a6afd93aab050ad1
ommiputera <omiputerakarunia@gmail.com>
2023-06-29 06:41:30 +0700

    format

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,10 +1,10 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@reach/tabs'
-import { Link, type V2_MetaFunction } from '@remix-run/react'
-import { SectionSpacer } from '~/components/spacer'
-import { getImgProps, images } from '~/images'
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from '@reach/tabs'
+import {Link, type V2_MetaFunction} from '@remix-run/react'
+import {SectionSpacer} from '~/components/spacer'
+import {getImgProps, images} from '~/images'
 
-export const meta: V2_MetaFunction = ({ matches }) => {
-  return [{ title: 'About Ommi Putera' }]
+export const meta: V2_MetaFunction = ({matches}) => {
+  return [{title: 'About Ommi Putera'}]
 }
 
 export default function Index() {
@@ -20,7 +20,7 @@ export default function Index() {
 function AboutMe() {
   return (
     <div className="relative mx-auto grid w-full max-w-[1800px] grid-cols-12 gap-x-16">
-      <div className="max-w-[1090px] col-span-4">
+      <div className="col-span-4 max-w-[1090px]">
         <img
           id="about-me"
           className="object-cover"
@@ -30,62 +30,64 @@ function AboutMe() {
           })}
         />
       </div>
-      <div className="col-span-8 grid grid-cols-5 w-full lg:gap-x-16">
+      <div className="col-span-8 grid w-full grid-cols-5 lg:gap-x-16">
         <div className="col-span-3 flex gap-x-16">
-          <div className='flex flex-col gap-y-6'>
-            <p className="font-light text-md text-white text-justify">
-              I help companies from all over the world with tailor-made solutions.
-              With each project, I push my work to new horizons, always putting
-              quality first. I help companies from all over the world with tailor-made solutions.
-              With each project, I push my work to new horizons, always putting
-              quality first.
+          <div className="flex flex-col gap-y-6">
+            <p className="text-justify text-md font-light text-white">
+              I help companies from all over the world with tailor-made
+              solutions. With each project, I push my work to new horizons,
+              always putting quality first. I help companies from all over the
+              world with tailor-made solutions. With each project, I push my
+              work to new horizons, always putting quality first.
             </p>
-            <p className="font-light text-md text-white text-justify">
-              I help companies from all over the world with tailor-made solutions.
-              With each project, I push my work to new horizons, always putting
-              quality first. I help companies from all over the world with tailor-made solutions.
-              With each project, I push my work to new horizons, always putting
-              quality first.
+            <p className="text-justify text-md font-light text-white">
+              I help companies from all over the world with tailor-made
+              solutions. With each project, I push my work to new horizons,
+              always putting quality first. I help companies from all over the
+              world with tailor-made solutions. With each project, I push my
+              work to new horizons, always putting quality first.
             </p>
           </div>
           <div>
             <h3 className="text-lg font-medium lg:text-xl">
               Here are some of the values I live by.
             </h3>
-            <p className="font-light text-md mt-4 text-white text-justify">
-              I help companies from all over the world with tailor-made solutions.
-              With each project, I push my work to new horizons, always putting
-              quality first. I help companies from all over the world with tailor-made solutions.
-              With each project, I push my work to new horizons, always putting
-              quality first.
+            <p className="mt-4 text-justify text-md font-light text-white">
+              I help companies from all over the world with tailor-made
+              solutions. With each project, I push my work to new horizons,
+              always putting quality first. I help companies from all over the
+              world with tailor-made solutions. With each project, I push my
+              work to new horizons, always putting quality first.
             </p>
           </div>
         </div>
-        <div className="col-span-2 sticky top-12 flex flex-col gap-y-8 h-fit">
-          <div className='bg-gray-800 xl:mr-16 2xl:mr-4 p-10'>
+        <div className="sticky top-12 col-span-2 flex h-fit flex-col gap-y-8">
+          <div className="2xl:mr-4 bg-gray-800 p-10 xl:mr-16">
             <div>
-              <p className="font-bold text-md text-white">
-                " I help companies from all over the world with tailor-made solutions.
-                With each project, I push my work to new horizons, always putting
-                quality first. "
+              <p className="text-md font-bold text-white">
+                " I help companies from all over the world with tailor-made
+                solutions. With each project, I push my work to new horizons,
+                always putting quality first. "
               </p>
               <SectionSpacer size="xs" />
-              <div className=''>
-                <h4 className='tracking-[0.15em] text-gray-300 font-light text-xs'>TESTIMONIAL BY</h4>
-                <p className='text-sm font-light mt-1 tracking-wide'>Naufal Ghifari</p>
+              <div className="">
+                <h4 className="text-xs font-light tracking-[0.15em] text-gray-300">
+                  TESTIMONIAL BY
+                </h4>
+                <p className="mt-1 text-sm font-light tracking-wide">
+                  Naufal Ghifari
+                </p>
               </div>
             </div>
             <SectionSpacer withoutBorder />
             <Tabs>
-              <TabList className='max-w flex gap-x-6'>
-                <Tab className='w-full pb-2'>Social</Tab>
-                <Tab className='w-full pb-2'>Contact</Tab>
-                <Tab className='w-full pb-2'>Resume</Tab>
+              <TabList className="max-w flex gap-x-6">
+                <Tab className="w-full pb-2">Social</Tab>
+                <Tab className="w-full pb-2">Contact</Tab>
+                <Tab className="w-full pb-2">Resume</Tab>
               </TabList>
-              <TabPanels className='mt-4'>
-                <TabPanel>
-                  instagram
-                </TabPanel>
+              <TabPanels className="mt-4">
+                <TabPanel>instagram</TabPanel>
               </TabPanels>
             </Tabs>
           </div>
