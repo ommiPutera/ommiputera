4a378484224e2299b422e5fd340f3d9b8418997e
ommiputera <omiputerakarunia@gmail.com>
2023-07-01 05:09:49 +0700

    update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,12 +6,12 @@ import {
   MenuPopover,
   useMenuButtonContext,
 } from '@reach/menu-button'
-import {Link, useLocation} from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import clsx from 'clsx'
-import {AnimatePresence, motion, useReducedMotion} from 'framer-motion'
-import {capitalize} from 'lodash'
-import {BurgerMenu} from '~/utils/icons'
-import {useRootData} from '~/utils/use-root-data'
+import { AnimatePresence, motion, useReducedMotion } from 'framer-motion'
+import { capitalize } from 'lodash'
+import { BurgerMenu } from '~/utils/icons'
+import { useRootData } from '~/utils/use-root-data'
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -22,6 +22,7 @@ import {
   NavigationMenuViewport,
 } from '~/components/shadcn/navigation-menu'
 import React from 'react'
+import { SectionSpacer } from './spacer'
 
 type TypeLinks = {
   name: string
@@ -34,7 +35,7 @@ type TypeLinks = {
 
 const RowApp1 = () => {
   return (
-    <div className="grid w-[420px] grid-cols-2 gap-x-2">
+    <div className="grid w-[420px] grid-cols-2 gap-x-2 px-3 pb-3.5">
       <Link to="/about" prefetch="intent">
         <div className="col-span-1 w-full rounded-lg px-3 pb-2.5 pt-2 hover:bg-gray-800">
           <p className="text-lg font-medium">Finance App</p>
@@ -55,17 +56,33 @@ const RowApp1 = () => {
   )
 }
 
-const App2 = () => {
+const RowProducts1 = () => {
   return (
-    <div className="grid w-[320px] grid-cols-2 gap-x-2">
-      <Link to="/about" prefetch="intent">
-        <div className="col-span-1 w-full rounded-lg px-3 pb-2.5 pt-2 hover:bg-gray-800">
-          <p className="text-lg font-medium">Finance App</p>
-          <p className="mt-1 text-md font-light leading-tight text-gray-300">
-            A collection of links for navigating websites.
-          </p>
-        </div>
-      </Link>
+    <div className="grid w-[620px] h-full grid-cols-2 gap-x-3 px-3 pb-3">
+      <div className='col-span-1 bg-gray-800 h-full flex flex-col justify-end rounded-lg p-4'>
+        <p className="text-lg bottom-2 text-xl font-bold">twon.com</p>
+        <p className="mt-2 text-md font-light leading-tight text-gray-300">
+          A collection of links for navigating websites. A collection of links for navigating websites.
+        </p>
+      </div>
+      <div className='col-span-1'>
+        <Link to="/about" prefetch="intent">
+          <div className="w-full rounded-lg px-3 pb-2.5 pt-2 hover:bg-gray-800">
+            <p className="text-lg font-medium">Finance App</p>
+            <p className="mt-1 text-md font-light leading-tight text-gray-300">
+              A collection of links for navigating websites. A collection of links for navigating websites.
+            </p>
+          </div>
+        </Link>
+        <Link to="/about" prefetch="intent">
+          <div className="w-full rounded-lg px-3 pb-2.5 pt-2 hover:bg-gray-800">
+            <p className="text-lg font-medium">Finance App</p>
+            <p className="mt-1 text-md font-light leading-tight text-gray-300">
+              A collection of links for navigating websites. A collection of links for navigating websites.
+            </p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
@@ -74,21 +91,30 @@ const LINKS: TypeLinks = [
   {
     name: 'Web Applications',
     asParent: true,
-    child: [{component: <RowApp1 />}],
+    child: [{ component: <RowApp1 /> }],
   },
-  {name: 'Project', to: '/project', asParent: false},
-  {name: 'About', to: '/about', asParent: false},
+  {
+    name: 'Products',
+    asParent: true,
+    child: [
+      { component: <RowProducts1 /> },
+      { component: <SectionSpacer size='xs' className='mx-3 mt-1' /> },
+      { component: <RowProducts1 /> }
+    ],
+  },
+  { name: 'Project', to: '/project', asParent: false },
+  { name: 'About', to: '/about', asParent: false },
 ]
 
 const OWNERLINKS = [
-  {name: 'Dashboard', to: '/dashboard'},
-  {name: 'Admin Panel', to: '/admin'},
+  { name: 'Dashboard', to: '/dashboard' },
+  { name: 'Admin Panel', to: '/admin' },
 ]
 
-const MOBILE_LINKS = [{name: 'Home', to: '/', asParent: false}, ...LINKS]
+const MOBILE_LINKS = [{ name: 'Home', to: '/', asParent: false }, ...LINKS]
 
 function Index() {
-  const {user} = useRootData()
+  const { user } = useRootData()
   return (
     <>
       <div
@@ -112,7 +138,7 @@ function MobileNav() {
     <div className="flex items-center justify-center lg:hidden">
       <div className="block">
         <Menu>
-          {({isExpanded}) => {
+          {({ isExpanded }) => {
             const state = isExpanded ? 'open' : 'closed'
             return (
               <>
@@ -130,9 +156,9 @@ function MobileNav() {
 }
 
 function MobileMenuList() {
-  const {isExpanded} = useMenuButtonContext()
+  const { isExpanded } = useMenuButtonContext()
   const shouldReduceMotion = useReducedMotion()
-  const {user} = useRootData()
+  const { user } = useRootData()
   return (
     <AnimatePresence>
       {isExpanded ? (
@@ -143,17 +169,17 @@ function MobileMenuList() {
             bottom: 0,
             right: 0,
           })}
-          style={{display: 'block'}}
+          style={{ display: 'block' }}
           className="z-50"
         >
           <motion.div
-            initial={{y: -50, opacity: 0}}
-            animate={{y: 0, opacity: 1}}
-            exit={{y: -50, opacity: 0}}
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -50, opacity: 0 }}
             transition={{
-              opacity: {duration: shouldReduceMotion ? 0 : 0.2},
-              rotate: {duration: shouldReduceMotion ? 0 : 0.5},
-              scale: {duration: shouldReduceMotion ? 0 : 0.5},
+              opacity: { duration: shouldReduceMotion ? 0 : 0.2 },
+              rotate: { duration: shouldReduceMotion ? 0 : 0.5 },
+              scale: { duration: shouldReduceMotion ? 0 : 0.5 },
               ease: 'linear',
             }}
             className="bg-primary fixed flex h-full w-full flex-col overflow-y-scroll pb-12 dark:border-gray-600"
@@ -221,15 +247,16 @@ function MobileNavLink({
 }
 
 function DesktopNav() {
-  const {user} = useRootData()
-  const [value, setValue] = React.useState('')
+  const { user } = useRootData()
+  const [open, setOpen] = React.useState('')
   return (
     <ul className="-mr-5 hidden lg:flex lg:items-center">
-      <NavigationMenu value={value} onValueChange={setValue}>
+      <NavigationMenu value={open} onValueChange={setOpen}>
         <NavigationMenuList>
           {LINKS.map(link => (
             <DesktopNavLink
-              closeContent={() => setValue('')}
+              isOpen={Boolean(open)}
+              closeContent={() => setOpen('')}
               key={link.to}
               to={link.to}
               child={link?.child}
@@ -261,13 +288,15 @@ function DesktopNavLink({
   to,
   child,
   asParent,
+  isOpen,
   closeContent,
   children,
   ...rest
 }: Omit<Parameters<typeof Link>['0'], 'to'> & {
   to?: string
-  child?: {component: string | React.ReactNode}[]
+  child?: { component: string | React.ReactNode }[]
   closeContent: () => void
+  isOpen: boolean
   asParent: boolean
 }) {
   const location = useLocation()
@@ -281,14 +310,14 @@ function DesktopNavLink({
           className={clsx(
             'w-full whitespace-nowrap text-lg font-medium focus:outline-none lg:tracking-wide',
             {
-              active: isSelected,
-              'text-secondary': !isSelected,
+              active: isOpen,
+              'text-secondary': !isOpen,
             },
           )}
         >
           {children}
         </NavigationMenuTrigger>
-        <NavigationMenuContent className="w-full bg-gray-900 px-3 pb-3.5 pt-3">
+        <NavigationMenuContent className="w-full bg-gray-900 pt-3">
           {child.map((link, index) => (
             <div key={index} onClick={closeContent}>
               <NavigationMenuLink asChild key={index}>
@@ -343,7 +372,7 @@ function Logo() {
 }
 
 function ProtectedNav() {
-  const {user} = useRootData()
+  const { user } = useRootData()
   if (!user) return <></>
   return (
     <div className="no-scrollbar overflow-y-hidden overflow-x-scroll border-b border-gray-600 bg-black px-5vw lg:px-10vw">
@@ -370,7 +399,7 @@ function ProtectedpNavLink({
   to,
   children,
   ...rest
-}: Omit<Parameters<typeof Link>['0'], 'to'> & {to: string}) {
+}: Omit<Parameters<typeof Link>['0'], 'to'> & { to: string }) {
   const location = useLocation()
   const isSelected =
     to === location.pathname || location.pathname.startsWith(`${to}/`)
@@ -394,4 +423,4 @@ function ProtectedpNavLink({
   )
 }
 
-export {Index as Navbar}
+export { Index as Navbar }
