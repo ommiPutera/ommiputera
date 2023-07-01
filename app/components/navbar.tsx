8d311764af1222d59d26982edcbe5514598dbc44
ommiputera <omiputerakarunia@gmail.com>
2023-07-02 00:55:27 +0700

    update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,12 +6,12 @@ import {
   MenuPopover,
   useMenuButtonContext,
 } from '@reach/menu-button'
-import {Link, useLocation} from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import clsx from 'clsx'
-import {AnimatePresence, motion, useReducedMotion} from 'framer-motion'
-import {capitalize} from 'lodash'
-import {BurgerMenu} from '~/utils/icons'
-import {useRootData} from '~/utils/use-root-data'
+import { AnimatePresence, motion, useReducedMotion } from 'framer-motion'
+import { capitalize } from 'lodash'
+import { BurgerMenu } from '~/utils/icons'
+import { useRootData } from '~/utils/use-root-data'
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -22,7 +22,8 @@ import {
   NavigationMenuViewport,
 } from '~/components/shadcn/navigation-menu'
 import React from 'react'
-import {SectionSpacer} from './spacer'
+import { SectionSpacer } from './spacer'
+import { Badge } from './shadcn/badge'
 
 type TypeLinks = {
   name: string
@@ -34,19 +35,26 @@ type TypeLinks = {
 }[]
 
 const RowApp1 = () => {
+  const isSelected = '/cash-flow' === location.pathname
+
   return (
-    <div className="grid w-[420px] grid-cols-2 gap-x-2 px-3 pb-3.5">
-      <Link to="/about" prefetch="intent">
-        <div className="col-span-1 w-full rounded-lg px-3 pb-2.5 pt-2 hover:bg-gray-800">
-          <p className="text-lg font-medium">Finance App</p>
-          <p className="mt-1 text-md font-light leading-tight text-gray-300">
-            A collection of links for navigating websites.
-          </p>
+    <div className="grid w-[420px] grid-cols-1 gap-x-2 px-3 pb-3.5">
+      <Link
+        to="/cash-flow"
+        prefetch="intent"
+        className={clsx('flex col-span-1 w-full rounded-lg pr-3 items-center hover:bg-gray-800', {
+          'bg-gray-800': isSelected
+        })}
+      >
+        <div className='bg-gradient-to-br pt-6 px-4 pb-4 from-orange-100 to-orange-300 border border-orange-200 mr-5 h-full rounded-l-md w-[120px]'>
+          <p className='text-2xl'>🎷</p>
+          <h4 className='text-md font-bold leading-tight'>Lean <br /> Cashflow</h4>
         </div>
-      </Link>
-      <Link to="/about" prefetch="intent">
-        <div className="col-span-1 rounded-lg px-3 pb-2.5 pt-2 hover:bg-gray-800">
-          <p className="text-lg font-medium">Finance Appsss</p>
+        <div className='mb-2 pb-2.5 pt-2'>
+          <div className='flex gap-x-2.5 items-center'>
+            <p className="text-lg font-medium">Personal Cash Flow</p>
+            <Badge variant="success">Free</Badge>
+          </div>
           <p className="mt-1 text-md font-light leading-tight text-gray-300">
             A collection of links for navigating websites.
           </p>
@@ -94,30 +102,30 @@ const LINKS: TypeLinks = [
   {
     name: 'Web Applications',
     asParent: true,
-    child: [{component: <RowApp1 />}],
+    child: [{ component: <RowApp1 /> }],
   },
   {
     name: 'Products',
     asParent: true,
     child: [
-      {component: <RowProducts1 />},
-      {component: <SectionSpacer size="xs" className="mx-3 mt-1" />},
-      {component: <RowProducts1 />},
+      { component: <RowProducts1 /> },
+      { component: <SectionSpacer size="xs" className="mx-3 mt-1" /> },
+      { component: <RowProducts1 /> },
     ],
   },
-  {name: 'Project', to: '/project', asParent: false},
-  {name: 'About', to: '/about', asParent: false},
+  { name: 'Project', to: '/project', asParent: false },
+  { name: 'About', to: '/about', asParent: false },
 ]
 
 const OWNERLINKS = [
-  {name: 'Dashboard', to: '/dashboard'},
-  {name: 'Admin Panel', to: '/admin'},
+  { name: 'Dashboard', to: '/dashboard' },
+  { name: 'Admin Panel', to: '/admin' },
 ]
 
-const MOBILE_LINKS = [{name: 'Home', to: '/', asParent: false}, ...LINKS]
+const MOBILE_LINKS = [{ name: 'Home', to: '/', asParent: false }, ...LINKS]
 
 function Index() {
-  const {user} = useRootData()
+  const { user } = useRootData()
   return (
     <>
       <div
@@ -141,7 +149,7 @@ function MobileNav() {
     <div className="flex items-center justify-center lg:hidden">
       <div className="block">
         <Menu>
-          {({isExpanded}) => {
+          {({ isExpanded }) => {
             const state = isExpanded ? 'open' : 'closed'
             return (
               <>
@@ -159,9 +167,9 @@ function MobileNav() {
 }
 
 function MobileMenuList() {
-  const {isExpanded} = useMenuButtonContext()
+  const { isExpanded } = useMenuButtonContext()
   const shouldReduceMotion = useReducedMotion()
-  const {user} = useRootData()
+  const { user } = useRootData()
   return (
     <AnimatePresence>
       {isExpanded ? (
@@ -172,17 +180,17 @@ function MobileMenuList() {
             bottom: 0,
             right: 0,
           })}
-          style={{display: 'block'}}
+          style={{ display: 'block' }}
           className="z-50"
         >
           <motion.div
-            initial={{y: -50, opacity: 0}}
-            animate={{y: 0, opacity: 1}}
-            exit={{y: -50, opacity: 0}}
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -50, opacity: 0 }}
             transition={{
-              opacity: {duration: shouldReduceMotion ? 0 : 0.2},
-              rotate: {duration: shouldReduceMotion ? 0 : 0.5},
-              scale: {duration: shouldReduceMotion ? 0 : 0.5},
+              opacity: { duration: shouldReduceMotion ? 0 : 0.2 },
+              rotate: { duration: shouldReduceMotion ? 0 : 0.5 },
+              scale: { duration: shouldReduceMotion ? 0 : 0.5 },
               ease: 'linear',
             }}
             className="bg-primary fixed flex h-full w-full flex-col overflow-y-scroll pb-12 dark:border-gray-600"
@@ -250,7 +258,7 @@ function MobileNavLink({
 }
 
 function DesktopNav() {
-  const {user} = useRootData()
+  const { user } = useRootData()
   const [open, setOpen] = React.useState('')
   return (
     <ul className="-mr-5 hidden lg:flex lg:items-center">
@@ -269,7 +277,7 @@ function DesktopNav() {
             </DesktopNavLink>
           ))}
         </NavigationMenuList>
-        <NavigationMenuViewport />
+        <NavigationMenuViewport className='bg-gray-900' />
       </NavigationMenu>
       {user && (
         <li className="px-5 py-2">
@@ -297,7 +305,7 @@ function DesktopNavLink({
   ...rest
 }: Omit<Parameters<typeof Link>['0'], 'to'> & {
   to?: string
-  child?: {component: string | React.ReactNode}[]
+  child?: { component: string | React.ReactNode }[]
   closeContent: () => void
   isOpen: boolean
   asParent: boolean
@@ -367,7 +375,7 @@ function Logo() {
 }
 
 function ProtectedNav() {
-  const {user} = useRootData()
+  const { user } = useRootData()
   if (!user) return <></>
   return (
     <div className="no-scrollbar overflow-y-hidden overflow-x-scroll border-b border-gray-600 bg-black px-5vw lg:px-10vw">
@@ -394,7 +402,7 @@ function ProtectedpNavLink({
   to,
   children,
   ...rest
-}: Omit<Parameters<typeof Link>['0'], 'to'> & {to: string}) {
+}: Omit<Parameters<typeof Link>['0'], 'to'> & { to: string }) {
   const location = useLocation()
   const isSelected =
     to === location.pathname || location.pathname.startsWith(`${to}/`)
@@ -418,4 +426,4 @@ function ProtectedpNavLink({
   )
 }
 
-export {Index as Navbar}
+export { Index as Navbar }
---
+++ app/routes/cash-flow/route.tsx
@@ -0,0 +1,27 @@
+import { type V2_MetaFunction } from '@remix-run/react'
+import { UIButton } from '~/components/shadcn/button'
+
+export const meta: V2_MetaFunction = ({ matches }) => {
+  return [{ title: 'Cash Flow Managament' }]
+}
+
+export default function Index() {
+  return (
+    <main className="flex flex-col gap-5 pb-44 lg:gap-24 lg:pt-8">
+      <div className="px-5vw lg:px-10vw">
+        <div className="text-primary relative mx-auto grid w-full max-w-8xl gap-5 py-4 lg:gap-y-16 lg:py-12">
+          <h1 className="text-center text-2xl font-medium leading-tight lg:px-[5rem] lg:text-7xl xl:px-[10rem]">
+            Manage and tracking <br /> your personal cashflow
+          </h1>
+          <h4 className="mt-2 px-0 text-center text-md font-medium leading-snug text-gray-400 lg:px-[20rem] lg:text-xl lg:text-gray-200">
+            First of all, thank you for interested in reading about me, On this
+            page, I will tell all about my life and my experiences
+          </h4>
+          <div className='mx-auto'>
+            <UIButton size="md">Login first</UIButton>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
