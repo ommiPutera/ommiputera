df972ba4eaf0cceb108b6f04e5dda09f2b892137
ommiputera <omiputerakarunia@gmail.com>
2023-07-06 23:07:26 +0700

    fix back btn

--- app/routes/cash-flow.monthly.tsx
+++ app/routes/cash-flow.monthly.tsx
@@ -1,29 +1,28 @@
-import type {V2_MetaFunction} from '@remix-run/react'
-import {Outlet} from '@remix-run/react'
-import {MoveLeftIcon} from 'lucide-react'
-import {useNavigate} from '@remix-run/react'
-import {UIButton} from '~/components/shadcn/button'
+import type { V2_MetaFunction } from '@remix-run/react'
+import { Link, Outlet } from '@remix-run/react'
+import { MoveLeftIcon } from 'lucide-react'
+import { UIButton } from '~/components/shadcn/button'
 
 export const meta: V2_MetaFunction = () => {
-  return [{title: 'Cashflow'}]
+  return [{ title: 'Cashflow' }]
 }
 
 export default function Index() {
-  const navigate = useNavigate()
   return (
     <>
       <div className="absolute -z-10 h-[50vh] w-screen bg-red-900 bg-gradient-to-b from-black to-gray-900"></div>
       <div className="w-full px-[4vw] xl:px-10vw">
         <div className="relative mx-auto flex max-w-7xl items-center justify-between py-9 lg:pb-12 lg:pt-12">
           <div className="w-full">
-            <UIButton
-              onClick={() => navigate(-1)}
-              variant="subtle"
-              className="text-md text-orange-500"
-            >
-              <MoveLeftIcon className="mr-2.5" size="20" />
-              <p> Back to cashflow</p>
-            </UIButton>
+            <Link to="/cash-flow" prefetch='intent'>
+              <UIButton
+                variant="subtle"
+                className="text-md text-orange-500"
+              >
+                <MoveLeftIcon className="mr-2.5" size="20" />
+                <p> Back to cashflow</p>
+              </UIButton>
+            </Link>
             <Outlet />
           </div>
         </div>
