dc18c89992b27014855cd1d8b5b41686365ec92e
ommiputera <omiputerakarunia@gmail.com>
2023-07-26 15:58:22 +0700

    format

--- app/routes/cash-flow.monthly.$id/route.tsx
+++ app/routes/cash-flow.monthly.$id/route.tsx
@@ -1,24 +1,21 @@
-import type { Post } from '@prisma/client'
-import type {
-  LoaderArgs,
-  ActionFunction,
-} from '@remix-run/node';
-import {
-  redirect,
-} from '@remix-run/node'
-import {
-  Form,
-  useActionData,
-  useLoaderData,
-} from '@remix-run/react'
+import type {Post} from '@prisma/client'
+import type {LoaderArgs, ActionFunction} from '@remix-run/node'
+import {redirect} from '@remix-run/node'
+import {Form, useActionData, useLoaderData} from '@remix-run/react'
 import React from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
-import { useDebouncedCallback } from 'use-debounce'
+import {useDebouncedCallback} from 'use-debounce'
 import Editor from '~/components/editor'
-import { createPost, deletePost, getPost, updateContent, updateTitle } from '~/utils/post.session'
-import { useUser } from '~/utils/use-root-data'
-import { Header } from './misc'
-import type { JSONContent } from '@tiptap/core'
+import {
+  createPost,
+  deletePost,
+  getPost,
+  updateContent,
+  updateTitle,
+} from '~/utils/post.session'
+import {useUser} from '~/utils/use-root-data'
+import {Header} from './misc'
+import type {JSONContent} from '@tiptap/core'
 
 export type SaveStatus = 'Saved' | 'Unsaved' | 'Saving..'
 type LoaderData = {
@@ -46,42 +43,39 @@ export enum FormType {
   DELETE = 'DELETE',
 }
 
-export const loader = async ({ request, params }: LoaderArgs) => {
-  const { id } = params;
-  const post = await getPost({ id: id ?? '' })
-  if (id === 'new') return { postId: id, isNewPage: true }
+export const loader = async ({request, params}: LoaderArgs) => {
+  const {id} = params
+  const post = await getPost({id: id ?? ''})
+  if (id === 'new') return {postId: id, isNewPage: true}
 
   if (!id || !post) return redirect('/cash-flow/monthly')
-  const data: LoaderData = { post, postId: id, isNewPage: false }
+  const data: LoaderData = {post, postId: id, isNewPage: false}
   return data
 }
 
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({request}) => {
   const formData = await request.formData()
-  const { _action, postId, title, authorId, postJSON } =
-    Object.fromEntries(formData);
+  const {_action, postId, title, authorId, postJSON} =
+    Object.fromEntries(formData)
 
   switch (_action) {
     case FormType.DELETE: {
       if (typeof postId !== 'string') {
-        return { formError: `Form not submitted correctly.` }
+        return {formError: `Form not submitted correctly.`}
       }
-      await deletePost({ id: postId })
+      await deletePost({id: postId})
       return redirect('/cash-flow/monthly', {})
     }
     case FormType.CREATE: {
-      if (
-        typeof authorId !== 'string' ||
-        typeof postJSON !== 'string'
-      ) {
-        return { formError: `Form not submitted correctly.` }
+      if (typeof authorId !== 'string' || typeof postJSON !== 'string') {
+        return {formError: `Form not submitted correctly.`}
       }
 
       let content
       if (postJSON) {
         content = JSON.parse(postJSON)
       } else {
-        content = JSON.parse(JSON.stringify({ blocks: ['none'] }))
+        content = JSON.parse(JSON.stringify({blocks: ['none']}))
       }
 
       return await createPost({
@@ -89,34 +83,28 @@ export const action: ActionFunction = async ({ request }) => {
         authorId,
         published: true,
         content,
-        redirectTo: '/cash-flow/monthly/'
+        redirectTo: '/cash-flow/monthly/',
       })
     }
     case FormType.UPDATE_TITLE: {
-      if (
-        typeof title !== 'string' ||
-        typeof postId !== 'string'
-      ) {
-        return { formError: `Form not submitted correctly.` }
+      if (typeof title !== 'string' || typeof postId !== 'string') {
+        return {formError: `Form not submitted correctly.`}
       }
       const post = await updateTitle({
         id: postId,
-        title
+        title,
       })
-      return { post };
+      return {post}
     }
     case FormType.UPDATE_CONTENT: {
-      if (
-        typeof postId !== 'string' ||
-        typeof postJSON !== 'string'
-      ) {
-        return { formError: `Form not submitted correctly.` }
+      if (typeof postId !== 'string' || typeof postJSON !== 'string') {
+        return {formError: `Form not submitted correctly.`}
       }
       let content
       if (postJSON) {
         content = JSON.parse(postJSON)
       } else {
-        content = JSON.parse(JSON.stringify({ blocks: ['none'] }))
+        content = JSON.parse(JSON.stringify({blocks: ['none']}))
       }
       return await updateContent({
         id: postId,
@@ -124,18 +112,20 @@ export const action: ActionFunction = async ({ request }) => {
       })
     }
     default: {
-      return { formError: `Action type invalid` }
+      return {formError: `Action type invalid`}
     }
   }
 }
 
 export default function Index() {
-  const { post, postId } = useLoaderData<LoaderData>()
+  const {post, postId} = useLoaderData<LoaderData>()
   const submitContentRef = React.useRef<HTMLInputElement>(null)
   const submitTitleRef = React.useRef<HTMLInputElement>(null)
   const [saveStatus, setSaveStatus] = React.useState<SaveStatus>('Saved')
   // @ts-ignore
-  const [content, setContent] = React.useState<JSONContent | null>(post?.content)
+  const [content, setContent] = React.useState<JSONContent | null>(
+    post?.content,
+  )
   const [pageTitle, setPageTitle] = React.useState(post?.title)
 
   const actionData = useActionData<ActionData>()
@@ -157,12 +147,15 @@ export default function Index() {
     }
   }, [])
 
-  const alertUser = React.useCallback((event: any) => {
-    event.preventDefault()
-    if (saveStatus === 'Unsaved') {
-      event.returnValue = ''
-    }
-  }, [saveStatus])
+  const alertUser = React.useCallback(
+    (event: any) => {
+      event.preventDefault()
+      if (saveStatus === 'Unsaved') {
+        event.returnValue = ''
+      }
+    },
+    [saveStatus],
+  )
 
   React.useEffect(() => {
     window.addEventListener('beforeunload', alertUser)
@@ -179,23 +172,22 @@ export default function Index() {
     }
   }, [alertUser, submitContent, submitTitleDebounce])
 
-
   return (
     <>
       <Header
-        type={postId === 'new' ? FormType.CREATE : (FormType.UPDATE_CONTENT)}
+        type={postId === 'new' ? FormType.CREATE : FormType.UPDATE_CONTENT}
         title={pageTitle}
         saveStatus={saveStatus}
         submitContent={submitContent}
       />
       <div className="mx-auto my-0 mt-12 flex w-full max-w-3xl flex-col items-center justify-between gap-y-6 py-4">
-        <Form method="POST" className='w-full' action='.'>
+        <Form method="POST" className="w-full" action=".">
           <div className="px-6 md:px-0">
             <TextareaAutosize
               autoFocus
               id="title-field"
               name="title"
-              onChange={(e) => {
+              onChange={e => {
                 submitTitleDebounce()
                 setPageTitle(e.target.value)
               }}
@@ -204,7 +196,11 @@ export default function Index() {
               className="w-full resize-none appearance-none overflow-hidden bg-transparent text-3xl font-bold leading-tight focus:outline-none lg:text-4xl"
             />
           </div>
-          <input type="hidden" name="_action" value={postId === 'new' ? FormType.CREATE : FormType.UPDATE_TITLE} />
+          <input
+            type="hidden"
+            name="_action"
+            value={postId === 'new' ? FormType.CREATE : FormType.UPDATE_TITLE}
+          />
           <input ref={submitTitleRef} type="submit" className="hidden" />
           <input type="hidden" name="authorId" value={user?.id} />
           <input
@@ -213,7 +209,7 @@ export default function Index() {
             value={postId ?? actionData?.post?.id}
           />
         </Form>
-        <Form method="POST" className='w-full'>
+        <Form method="POST" className="w-full">
           <div className="">
             <Editor
               content={content}
@@ -223,7 +219,11 @@ export default function Index() {
               submit={submitContentDebounce}
             />
           </div>
-          <input type="hidden" name="_action" value={postId === 'new' ? FormType.CREATE : FormType.UPDATE_CONTENT} />
+          <input
+            type="hidden"
+            name="_action"
+            value={postId === 'new' ? FormType.CREATE : FormType.UPDATE_CONTENT}
+          />
           <input type="hidden" name="authorId" value={user?.id} />
           <input ref={submitContentRef} type="submit" className="hidden" />
           <input
