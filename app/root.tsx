9ea1ffd5e5361504c336a3e201716ee0b9a7c02e
ommiputera <omiputerakarunia@gmail.com>
2023-08-08 07:57:56 +0700

    update prisma version

--- app/root.tsx
+++ app/root.tsx
@@ -15,31 +15,31 @@ import {
   useLoaderData,
   useNavigation,
 } from '@remix-run/react'
-import {Analytics} from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
 import * as React from 'react'
-import {useSpinDelay} from 'spin-delay'
-import {Navbar} from '~/components/navbar'
+import { useSpinDelay } from 'spin-delay'
+import { Navbar } from '~/components/navbar'
 import appStyles from '~/styles/app.css'
 import tailwindStyles from '~/styles/tailwind.css'
 import vendorsStyles from '~/styles/vendors.css'
 import prosemirrorStyles from '~/styles/prosemirror.css'
-import {ThemeProvider, useTheme} from '~/utils/theme-provider'
+import { ThemeProvider, useTheme } from '~/utils/theme-provider'
 import Footer from './components/footer'
-import {getDomainUrl, getUrl} from './utils/misc'
-import {getSocialMetas} from './utils/seo'
-import {Toaster} from './components/shadcn/toaster'
+import { getDomainUrl, getUrl } from './utils/misc'
+import { getSocialMetas } from './utils/seo'
+import { Toaster } from './components/shadcn/toaster'
 import clsx from 'clsx'
-import {useRootData} from './utils/use-root-data'
-import {getThemeSession} from './utils/theme.server'
-import {getUser} from './utils/session.server'
+import { useRootData } from './utils/use-root-data'
+import { getThemeSession } from './utils/theme.server'
+import { getUser } from './utils/session.server'
 
 export type LoaderData = SerializeFrom<typeof loader>
 
-export const handle: {id: string} = {
+export const handle: { id: string } = {
   id: 'root',
 }
 
-export async function loader({request}: DataFunctionArgs) {
+export async function loader({ request }: DataFunctionArgs) {
   const user = await getUser(request)
   const [themeSession] = await Promise.all([getThemeSession(request)])
   const data = {
@@ -52,10 +52,10 @@ export async function loader({request}: DataFunctionArgs) {
     },
   }
   const headers: HeadersInit = new Headers()
-  return json(data, {headers})
+  return json(data, { headers })
 }
 
-export const meta: V2_MetaFunction = ({data}) => {
+export const meta: V2_MetaFunction = ({ data }) => {
   const requestInfo = data?.requestInfo
   return [
     ...getSocialMetas({
@@ -90,10 +90,10 @@ export const links: LinksFunction = () => {
       type: 'font/woff2',
       crossOrigin: 'anonymous',
     },
-    {rel: 'stylesheet', href: vendorsStyles},
-    {rel: 'stylesheet', href: tailwindStyles},
-    {rel: 'stylesheet', href: prosemirrorStyles},
-    {rel: 'stylesheet', href: appStyles},
+    { rel: 'stylesheet', href: vendorsStyles },
+    { rel: 'stylesheet', href: tailwindStyles },
+    { rel: 'stylesheet', href: prosemirrorStyles },
+    { rel: 'stylesheet', href: appStyles },
   ]
 }
 
@@ -173,7 +173,7 @@ function PageLoadingMessage() {
 }
 
 function App() {
-  const {user} = useRootData()
+  const { user } = useRootData()
   const [theme] = useTheme()
   return (
     <html lang="en" className={`${theme}`}>
@@ -189,7 +189,6 @@ function App() {
           name="theme-color"
           content={theme === 'dark' ? '#161513' : '#FFF'}
         />
-        <meta name="robots" content="noindex, nofollow" />
         <Meta />
         <Links />
       </head>
