310f818539d349c456afaf083991d0dca469fdcd
ommiputera <omiputerakarunia@gmail.com>
2023-10-15 15:14:59 +0700

    format

--- app/routes/personal+/_layout.tsx
+++ app/routes/personal+/_layout.tsx
@@ -1,16 +1,16 @@
-import type { LoaderFunction } from '@remix-run/node'
-import { Outlet, useLocation } from '@remix-run/react'
+import type {LoaderFunction} from '@remix-run/node'
+import {Outlet, useLocation} from '@remix-run/react'
 import clsx from 'clsx'
 import React from 'react'
-import { ButtonLink } from '~/components/button'
-import { Profile } from '~/components/me'
-import { DarkModeToggle, Logo, MoreAction } from '~/components/navbar'
-import { requireUserSession } from '~/utils/session.server'
+import {ButtonLink} from '~/components/button'
+import {Profile} from '~/components/me'
+import {DarkModeToggle, Logo, MoreAction} from '~/components/navbar'
+import {requireUserSession} from '~/utils/session.server'
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({request}) => {
   const user = await requireUserSession(request)
   if (!user) {
-    throw new Response('Unauthorized', { status: 401 })
+    throw new Response('Unauthorized', {status: 401})
   }
   return {}
 }
@@ -26,7 +26,7 @@ export default function Index() {
         <div className="hidden max-w-[14rem] md:block">
           <Navigation />
         </div>
-        <div className="grid mx-auto w-full max-w-4xl xl:max-w-5xl grid-cols-12">
+        <div className="mx-auto grid w-full max-w-4xl grid-cols-12 xl:max-w-5xl">
           <Outlet />
         </div>
       </div>
@@ -67,7 +67,7 @@ export function OutletCenter({
     <div
       {...props}
       className={clsx(
-        'col-span-12 min-h-screen w-full border-l border-gray-100 dark:border-gray-800 lg:border-r lg:col-span-8 xl:col-span-9',
+        'col-span-12 min-h-screen w-full border-l border-gray-100 dark:border-gray-800 lg:col-span-8 lg:border-r xl:col-span-9',
       )}
     >
       {children}
@@ -82,7 +82,7 @@ export function OutletRight({
   children: JSX.Element | React.ReactNode
 }) {
   return (
-    <div className="hidden w-full lg:col-span-4 xl:col-span-3 lg:block">
+    <div className="hidden w-full lg:col-span-4 lg:block xl:col-span-3">
       <div
         {...props}
         className={clsx(
@@ -108,11 +108,11 @@ export function OutletRight({
 
 function NavbarMenu() {
   return (
-    <div className="flex flex-col gap-8 justify-center py-5">
+    <div className="flex flex-col justify-center gap-8 py-5">
       <div className="my-2 flex flex-col gap-8">
         <Logo size="md" />
       </div>
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <NavbarItem title="Beranda" route="/personal" icon="/icons/home.png" />
       </div>
     </div>
@@ -139,14 +139,20 @@ function NavbarItem({
       prefetch="intent"
       align="left"
       className={clsx(
-        'flex items-center gap-4 px-0 py-2 group hover:text-black hover:dark:text-white',
+        'group flex items-center gap-4 px-0 py-2 hover:text-black hover:dark:text-white',
         {
           'text-gray-300 dark:text-gray-200': !isSelected,
           'text-black dark:text-white': isSelected,
         },
       )}
     >
-      <img src={icon} alt="" className={clsx("h-5 w-5 group-hover:grayscale-0", { 'grayscale': !isSelected, })} />
+      <img
+        src={icon}
+        alt=""
+        className={clsx('h-5 w-5 group-hover:grayscale-0', {
+          grayscale: !isSelected,
+        })}
+      />
       <h2 className="pb-0.5 text-md font-medium">{title}</h2>
     </ButtonLink>
   )
