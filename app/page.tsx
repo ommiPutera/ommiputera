bb7cac4a6b061250b860d3c01ed2fadb451b63d4
ommiputera <omiputerakarunia@gmail.com>
2024-11-22 11:33:04 +0700

    update hhih

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,59 @@
+import Image from "next/image";
+import { CornerUpRight } from "lucide-react";
+
 import ShellPage from "~/components/shell-page";
+import { Carousel, CarouselContent, CarouselItem } from "~/components/ui/carousel";
 
 export default function Home() {
   return (
     <ShellPage>
-      <p>Hello Guys</p>
+      <About />
+      <div className="flex flex-col p-6 overflow-hidden">
+        flexx
+      </div>
     </ShellPage>
   );
-}
+};
 
-{/* <div className="flex flex-col space-y-1 md:space-y-2">
-  <h2 className="text-2xl md:text-4xl font-extrabold tracking-tight">About</h2>
-  <p className="text-sm md:text-base font-normal text-accent-foreground leading-6">
-    At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, I pursued a double degree in computer science and business, interned at big tech companies in Silicon Valley, and competed in over 21 hackathons for fun. I also had the pleasure of being a part of the first ever in-person cohort of buildspace called buildspace sf1.
-  </p>
-</div> */}
+function About() {
+  return (
+    <div className="flex flex-col p-6 overflow-hidden">
+      <div className="flex items-center gap-2 md:gap-4">
+        <div className="border border-neutral-400 p-1 rounded-lg">
+          <CornerUpRight className="w-5 h-5" />
+        </div>
+        <h2 className="text-base md:text-xl font-extrabold tracking-tight">About</h2>
+      </div>
+      <div className="pl-[38px] md:pl-[46px] flex flex-col gap-4 mb-4">
+        <p className="text-sm md:text-base font-normal text-accent-foreground leading-5">
+          Highly motivated and results-driven Software Engineer with 3+ years of experience specializing in scalable frontend and backend development for fintech products.
+        </p>
+        <p className="text-sm md:text-base font-normal text-accent-foreground leading-5">
+          Proven ability to lead high-impact projects, build seamless user experiences, and contribute to a collaborative team environment. Expertise in React, Next.js, Remix, Node.js, and Nest.js, with a strong focus on stability, performance, and security.
+        </p>
+      </div>
+      <div className="pl-[38px] md:pl-[46px]">
+        <Carousel>
+          <CarouselContent overflowVisible className="-ml-1">
+            <CarouselItem className="pl-1 h-[310px] md:h-[420px] overflow-hidden">
+              <Image
+                src="/images/profile.jpeg"
+                width={500}
+                height={500}
+                alt=""
+              />
+            </CarouselItem>
+            <CarouselItem className="pl-1 h-[310px] md:h-[420px] overflow-hidden">
+              <Image
+                src="/images/ommi_original.jpg"
+                width={500}
+                height={500}
+                alt=""
+              />
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </div>
+  )
+}
