644629e867cceefd78502be848bb3c3011e9df05
ommiputera <omiputerakarunia@gmail.com>
2024-11-22 16:13:03 +0700

    update layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import { CornerUpRight } from "lucide-react";
 
 import ShellPage from "~/components/shell-page";
 import { Carousel, CarouselContent, CarouselItem } from "~/components/ui/carousel";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <ShellPage>
       <About />
-      <div className="flex flex-col p-6 overflow-hidden">
+      <div className="flex flex-col px-4 py-6 md:p-6 overflow-hidden">
         flexx
       </div>
     </ShellPage>
@@ -17,25 +18,31 @@ export default function Home() {
 
 function About() {
   return (
-    <div className="flex flex-col p-6 overflow-hidden">
+    <div className="flex flex-col px-4 py-6 md:p-6 overflow-hidden">
       <div className="flex items-center gap-2 md:gap-4">
-        <div className="border border-neutral-400 p-1 rounded-lg">
-          <CornerUpRight className="w-5 h-5" />
+        <div className="border border-neutral-500 p-1 rounded-lg">
+          <CornerUpRight className="w-4 h-4 md:w-5 md:h-5" />
         </div>
-        <h2 className="text-base md:text-xl font-extrabold tracking-tight">About</h2>
+        <h2 className="text-base md:text-xl font-bold tracking-tight">About</h2>
       </div>
-      <div className="pl-[38px] md:pl-[46px] flex flex-col gap-4 mb-4">
+      <div className="pt-4 md:pl-[46px] md:pt-0 flex flex-col gap-4 mb-4">
         <p className="text-sm md:text-base font-normal text-accent-foreground leading-5">
           Highly motivated and results-driven Software Engineer with 3+ years of experience specializing in scalable frontend and backend development for fintech products.
         </p>
         <p className="text-sm md:text-base font-normal text-accent-foreground leading-5">
-          Proven ability to lead high-impact projects, build seamless user experiences, and contribute to a collaborative team environment. Expertise in React, Next.js, Remix, Node.js, and Nest.js, with a strong focus on stability, performance, and security.
+          <span>
+            Proven ability to lead high-impact projects, build seamless user experiences, and contribute to a collaborative team environment.
+          </span>
+          <br />
+          <Link href="/about" className="font-medium text-blue-600">
+            Show more..
+          </Link>
         </p>
       </div>
-      <div className="pl-[38px] md:pl-[46px]">
+      <div className="md:pl-[46px]">
         <Carousel>
-          <CarouselContent overflowVisible className="-ml-1">
-            <CarouselItem className="pl-1 h-[310px] md:h-[420px] overflow-hidden">
+          <CarouselContent overflowVisible className="-ml-1.5">
+            <CarouselItem className="pl-1.5 overflow-hidden rounded-3xl">
               <Image
                 src="/images/profile.jpeg"
                 width={500}
@@ -43,9 +50,9 @@ function About() {
                 alt=""
               />
             </CarouselItem>
-            <CarouselItem className="pl-1 h-[310px] md:h-[420px] overflow-hidden">
+            <CarouselItem className="pl-1.5 overflow-hidden rounded-3xl">
               <Image
-                src="/images/ommi_original.jpg"
+                src="/images/profile.jpeg"
                 width={500}
                 height={500}
                 alt=""
--- components/shell-page.tsx
+++ components/shell-page.tsx
@@ -1,33 +1,55 @@
+import Link from "next/link"
+
 export default function ShellPage({ children }: { children: React.ReactNode }) {
   return (
     <div className="container mx-auto max-w-screen-sm">
       <main className="flex flex-col min-h-screen relative">
-        <div className="flex flex-col space-y-1 min-h-[var(--hero-height-mobile)] max-h-[var(--hero-height-mobile)] md:min-h-[var(--hero-height)] md:max-h-[var(--hero-height)] md:space-y-2 sticky top-0 pt-12 md:pt-20 bg-white z-10 px-4 md:px-14">
-          <h1 className="text-3xl md:text-5xl font-extrabold tracking-tight text-center">Hi, I&apos;m Ommi 👋</h1>
-          <p className="text-md md:text-xl font-normal leading-6 max-w-xs mx-auto text-center">Software Engineer based in Jakarta, Indonesia. I love building things with Remix.</p>
-          <div className=" relative pt-24">
-          </div>
+        <div className="z-10 flex flex-col space-y-1 min-h-[var(--hero-height-mobile)] bg-white md:min-h-[var(--hero-height)] md:space-y-2 sticky top-0 pt-12 md:pt-20">
+          <Intro />
+          <RoundedBorder />
         </div>
-        <div className="sticky top-[var(--hero-height-mobile)] md:top-[var(--hero-height)] w-full h-[1px] bg-neutral-300 z-10">
-          <div className="relative">
-            <div className="absolute top-0 left-0">
-              <div className="relative w-10 md:w-20 h-6 bg-white">
-                <div className="absolute top-0 right-0 w-6 h-6 border-l border-t border-neutral-300 rounded-tl-full"></div>
-              </div>
-            </div>
-            <div className="absolute top-0 right-0">
-              <div className="relative w-10 md:w-20 h-6 bg-white">
-                <div className="absolute top-0 left-0 w-6 h-6 border-r border-t border-neutral-300 rounded-tr-full"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="px-4 md:px-14 relative overflow-hidden">
-          <div className="bg-white border-l border-r divide-y divide-neutral-300 border-b border-neutral-300 relative min-h-[200vh]">
+        <div className="px-3 md:px-14 relative overflow-hidden">
+          <div className="bg-white border-x border-neutral-200 divide-y divide-neutral-200 relative min-h-[200vh]">
             {children}
           </div>
         </div>
       </main>
     </div>
   )
-} 
+}
+
+function Intro() {
+  return (
+    <div className="absolute top-0 px-3 md:px-14 bg-white w-full h-full pt-7 md:pt-16">
+      <h1 className="text-3xl md:text-5xl font-extrabold tracking-tight text-center">Hi, I&apos;m Ommi 👋</h1>
+      <p className="mt-1 md:mt-2 text-md md:text-xl font-normal leading-5 max-w-xs md:max-w-md mx-auto text-center">Software Engineer based in Jakarta, Indonesia. I love building things with Remix.</p>
+      <ul className="text-center flex justify-center gap-3 text-sm md:text-lg mt-4 md:mt-6">
+        <li className="underline">
+          <Link href="/">Github</Link>
+        </li>
+        <li className="underline">
+          <Link href="/">Resume</Link>
+        </li>
+        <li className="underline">
+          <Link href="/">Portfolio</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+function RoundedBorder() {
+  return (
+    <div>
+      <div className="w-full h-3 top-[calc(var(--hero-height-mobile)_-_11px)] md:top-[calc(var(--hero-height)_-_10px)] absolute overflow-hidden">
+        <div className="w-[calc(100%_-_100px)] md:w-[calc(100%_-_190px)] left-1/2 -translate-x-1/2 absolute bottom-0 border-b border-neutral-200"></div>
+      </div>
+      <div className="w-10 h-10 top-[var(--hero-height-mobile)] md:top-[var(--hero-height)] absolute overflow-hidden left-3 md:left-14">
+        <div className="absolute top-0 left-0 w-14 h-12 border border-neutral-200 rounded-tl-3xl shadow-circle"></div>
+      </div>
+      <div className="w-10 h-10 top-[var(--hero-height-mobile)] md:top-[var(--hero-height)] absolute overflow-hidden right-3 md:right-14">
+        <div className="absolute top-0 right-0 w-14 h-12 border border-neutral-200 rounded-tr-3xl shadow-circle"></div>
+      </div>
+    </div>
+  )
+}
