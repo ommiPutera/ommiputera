a2e586a4942b5e84d807927b43950f9fc21bec7e
ommiputera <omiputerakarunia@gmail.com>
2024-12-09 01:50:32 +0700

    update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Br from "~/components/br";
-import { ContentParagraph, ContentTitle } from "~/components/content";
+import { ContentParagraph } from "~/components/content";
 import Section from "~/components/section";
 import ShellPage from "~/components/shell-page";
 
 import { getBlogPosts, Metadata } from "~/data/blog";
 
-import { formatDate } from "~/lib/utils";
+import { cn, formatDate } from "~/lib/utils";
 
 export const metadata = {
   title: "The Ommi Putera Blog",
@@ -27,61 +27,55 @@ export default async function BlogPage() {
   return (
     <ShellPage title="The Ommi Putera Blog" withHome withBack>
       <Section>
-        <div className="flex gap-2">
-          <div className="w-full max-w-10">
-            <Image
-              src="/images/profile.jpeg"
-              width={40}
-              height={40}
-              alt=""
-              className="object-cover overflow-hidden rounded-full border border-neutral-200 dark:border-neutral-800"
-            />
-          </div>
-          <div className="w-full">
-            <ContentTitle title="Blog" description="Ommi Putera" />
-          </div>
+        <div className="my-4 overflow-hidden relative">
+          <h1 className="relative z-10 text-3xl md:text-4xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-500 to-neutral-950 dark:from-neutral-50 dark:to-neutral-300 text-center font-sans font-extrabold">
+            The Blog
+          </h1>
+          <ContentParagraph className="text-center text-sm md:text-base mx-6 md:mx-24 mt-1">
+            I write about the React ecosystem, TypeScript, and the lessons
+            I&apos;ve learned while tackling real-world challenges.
+          </ContentParagraph>
         </div>
-        <ContentParagraph className="mt-1">
-          I write about the React ecosystem, TypeScript, and the lessons
-          I&apos;ve learned while tackling real-world challenges.
-        </ContentParagraph>
         <Br />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-2">
-          {blogs.map((post) => {
+        <BentoGrid>
+          {blogs.map((post, index) => {
             const slug = post.slug;
             const source = post.source;
             return (
-              <Blog
+              <BentoBlog
                 key={post.slug}
                 source={source}
                 slug={slug}
+                index={index}
                 {...(post.metadata as Metadata)}
               />
             );
           })}
-        </div>
+        </BentoGrid>
         <Br />
       </Section>
     </ShellPage>
   );
 }
 
-function Blog({
+function BentoBlog({
   slug,
   publishedAt,
   image,
   summary,
   title,
+  index
 }: {
   slug: string;
   source: string;
   id?: string;
+  index: number;
 } & Metadata) {
   return (
     <Link
       href={`/blog/${slug}`}
       prefetch
-      className="rounded-xl bg-background w-full cursor-pointer block h-full md:max-w-[300px] overflow-hidden border border-neutral-200 dark:border-neutral-800"
+      className={cn("rounded-xl bg-white dark:bg-background w-full cursor-pointer block h-full overflow-hidden border border-neutral-200 dark:border-neutral-800", index === 3 || index === 6 ? "md:col-span-2" : "")}
     >
       <div className="relative">
         <Image
@@ -94,7 +88,7 @@ function Blog({
         <div className="absolute bottom-0 from-neutral-950 to-transparent bg-gradient-to-t w-full h-1/2"></div>
       </div>
       <div className="p-9 md:p-4 prose dark:prose-invert">
-        <h3 className="text-xl font-extrabold w-full leading-7">{title}</h3>
+        <h3 className="text-xl  font-extrabold w-full">{title}</h3>
         <p className="text-xs md:text-sm font-normal text-neutral-500 dark:text-neutral-300 inline-flex flex-col gap-2">
           <span>{formatDate(publishedAt)}</span>
           <span>{summary}</span>
@@ -103,3 +97,22 @@ function Blog({
     </Link>
   );
 }
+
+const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-2 mx-auto h-full",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
