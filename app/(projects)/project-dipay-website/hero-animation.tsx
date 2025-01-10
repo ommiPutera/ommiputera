c2b6a518c4601c462a5ebfc2d9eb673c0175930a
ommiputera <omiputerakarunia@gmail.com>
2025-01-10 13:24:23 +0700

    update

--- app/(projects)/project-dipay-website/hero-animation.tsx
+++ app/(projects)/project-dipay-website/hero-animation.tsx
@@ -1,18 +1,11 @@
 import { Play } from "lucide-react";
-import Video from 'next-video';
-
-import { list } from '@vercel/blob';
+import Player from 'next-video/player';
 
 import Br from "~/components/br";
 import Content, { ContentParagraph } from "~/components/content";
 import Section from "~/components/section";
 
 export default async function HeroAnimation() {
-  const { blobs } = await list({
-    prefix: "hero-animation",
-    limit: 1,
-  })
-  const { url } = blobs[0]
   return (
     <Section className="pt-0 md:pt-0" withConnector>
       <div className="border border-neutral-200 dark:border-neutral-800 h-10 bg-neutral-100 dark:bg-neutral-800 w-10 flex justify-center items-center rounded-full">
@@ -26,16 +19,15 @@ export default async function HeroAnimation() {
           </span>
         </ContentParagraph>
         <Br />
-        <Video
+        <Player
           loop
           autoPlay
           muted
-          controls={false}
+          // controls={false}
+          playsInline
+          src="https://ffg45va0vgszoqqt.public.blob.vercel-storage.com/hero-animation-0MsDZhsGWhcOXhMvRGwJBwbzL3H2DA.mp4"
           className="border overflow-hidden rounded-xl h-[calc(100vw_/_2.5)] md:h-[calc(var(--shell-page-width)_/_2.7)]"
-        >
-          <source src={url} type="video/mp4" />
-          Your browser does not support the video tag.
-        </Video>
+        />
       </Content>
     </Section>
   );
