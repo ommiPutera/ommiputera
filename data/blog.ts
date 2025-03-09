b1b4d3281bb96e60377eb6003a82ba5c963cea0d
ommiputera <omiputerakarunia@gmail.com>
2025-03-09 21:32:09 +0700

    update

--- data/blog.ts
+++ data/blog.ts
@@ -6,6 +6,12 @@ import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
+import { createHighlighterCore, type HighlighterCore } from "shiki/core";
+import { createOnigurumaEngine } from "shiki/engine/oniguruma";
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+} from "@shikijs/transformers";
 
 import { calculateReadingTime } from "~/utils/calculateReadingTime";
 
@@ -30,15 +36,45 @@ function getMDXFiles(dir: string): string[] {
   return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
 }
 
+let highlighter: HighlighterCore | null = null;
+
+async function getHighlighter() {
+  if (highlighter) return highlighter;
+
+  const [themes, langs] = await Promise.all([
+    import("@shikijs/themes/vitesse-light").then((mod) => mod.default),
+    import("@shikijs/langs/javascript").then((mod) => mod.default),
+  ]);
+
+  highlighter = await createHighlighterCore({
+    themes: [themes],
+    langs: [langs],
+    engine: createOnigurumaEngine(() => import("shiki/wasm")),
+  });
+
+  return highlighter;
+}
+
 export async function markdownToHTML(markdown: string): Promise<string> {
+  const highlighterInstance = await getHighlighter();
+  
   const processed = await unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeShiki, {
+      highlighter: highlighterInstance,
       themes: {
         light: "vitesse-light",
         dark: "vitesse-dark",
       },
+      transformers: [
+        transformerNotationHighlight({
+          matchAlgorithm: "v3",
+        }),
+        transformerNotationDiff({
+          matchAlgorithm: "v3",
+        }),
+      ],
     })
     .use(rehypeStringify)
     .process(markdown);
