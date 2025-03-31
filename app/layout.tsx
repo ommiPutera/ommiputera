bb1c7e0a4f6ded1d12a19711adaf5dab01a3fbdf
ommiputera <omiputerakarunia@gmail.com>
2025-04-01 00:22:24 +0700

    under construnction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 
-import { ThemeProvider } from "~/components/theme-provider";
-import { BackgroundBeams } from "~/components/ui/background-beams";
+// import { ThemeProvider } from "~/components/theme-provider";
+// import { BackgroundBeams } from "~/components/ui/background-beams";
 
-import NextTopLoader from "nextjs-toploader";
+// import NextTopLoader from "nextjs-toploader";
 
 import "./globals.css";
 
@@ -13,14 +13,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  // children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
-        <NextTopLoader color="#1878f24d" height={2} showSpinner={false} />
+        <h1>Under Construction</h1>
+        {/* <NextTopLoader color="#1878f24d" height={2} showSpinner={false} />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -29,7 +30,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
-        <BackgroundBeams className="fixed top-0 left-0 h-full w-full -z-10" />
+        <BackgroundBeams className="fixed top-0 left-0 h-full w-full -z-10" /> */}
       </body>
     </html>
   );
